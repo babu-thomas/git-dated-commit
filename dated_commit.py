14c2d415954772f232584f91b795385fc3fd6446
Babu Thomas <babuthomas138@gmail.com>
2018-09-13 17:42:05 +0530

    Refactor code into functions

--- dated_commit.py
+++ dated_commit.py
@@ -4,9 +4,22 @@ from pathlib import Path
 
 from dateutil.tz import gettz
 
-p = Path('.')
-files = [x.resolve() for x in p.iterdir() if not x.is_dir()]
-print(files)
-unix_times = [os.path.getmtime(x) for x in files]
-times = [dt.datetime.fromtimestamp(x, gettz('Asia/Calcutta')).isoformat() for x in unix_times]
-print(times)
+
+def get_file_paths(dir_):
+    if not dir_.is_dir:
+        return list(dir_.resolve())
+    return [path.resolve() for path in dir_.iterdir() if not path.is_dir()]
+
+
+def get_modified_time(files, timezone):
+    unix_times = [os.path.getmtime(f) for f in files]
+    return [dt.datetime.fromtimestamp(t, timezone).isoformat() for t in unix_times]
+
+
+if __name__ == '__main__':
+    p = Path('.')
+    paths = get_file_paths(p)
+    print(paths)
+    tz = gettz('Asia/Calcutta')
+    times = get_modified_time(paths, tz)
+    print(times)
