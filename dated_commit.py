0ed7013ed07168409cf2fe44c1db7cb4f4af80ff
Babu Thomas <babuthomas138@gmail.com>
2018-09-14 19:21:40 +0530

    Initialize git repository and add a file to it

--- dated_commit.py
+++ dated_commit.py
@@ -20,14 +20,26 @@ def get_modified_time(files):
 def execute_shell_command(cmd, work_dir):
     pipe = subprocess.Popen(cmd, shell=True, cwd=work_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, error = pipe.communicate()
+    print(out.decode(), error.decode())
     return out, error
 
 
+def git_init(path):
+    cmd = 'git init'
+    return execute_shell_command(cmd, path)
+
+
+def git_add(file_path, repo_path):
+    cmd = 'git add ' + str(file_path.resolve())
+    return execute_shell_command(cmd, repo_path)
+
+
 if __name__ == '__main__':
-    p = Path()
-    out, error = execute_shell_command('git --version', p)
-    print(out.decode(), error.decode())
+    p = Path('D:/code/interviewbit')
     paths = get_file_paths(p)
     print(paths)
     times = get_modified_time(paths)
     print(times)
+    execute_shell_command('git --version', p)
+    git_init(p)
+    git_add(paths[0], p)
