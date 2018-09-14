9fea5ffaae4daf2d4f11fed267daf4153314cd95
Babu Thomas <babuthomas138@gmail.com>
2018-09-14 20:20:35 +0530

    Commit a file with given date

--- dated_commit.py
+++ dated_commit.py
@@ -35,14 +35,18 @@ def git_add(file_path, repo_path):
     return execute_shell_command(cmd, repo_path)
 
 
+def git_commit_with_date(commit_msg, date, repo_path):
+    env = {'GIT_AUTHOR_DATE': date, 'GIT_COMMITTER_DATE': date}
+    cmd = f'git commit -m "{commit_msg}"'
+    return execute_shell_command(cmd, repo_path, env)
+
+
 if __name__ == '__main__':
     p = Path('D:/code/interviewbit')
     paths = get_file_paths(p)
     print(paths)
     times = get_modified_time(paths)
     print(times)
-    env = {'TEST_ENV_VAR': 'HELLO'}
-    execute_shell_command('set TEST_ENV_VAR', p, env_vars=env)
-    execute_shell_command('set TEST_ENV_VAR', p)
-    # git_init(p)
-    # git_add(paths[0], p)
+    git_init(p)
+    git_add(paths[0], p)
+    git_commit_with_date(paths[0].name, times[0], p)
